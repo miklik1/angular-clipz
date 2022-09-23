1120ec897c970d647cb305c934722c623839802f
martin_miklik <martin.miklik@brandmaster.com>
2022-09-23 15:50:54 +0200

    progress

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'app-nav',
@@ -11,8 +11,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 export class NavComponent implements OnInit {
   constructor(
     public modal: ModalService,
-    public auth: AuthService,
-    private afAuth: AngularFireAuth
+    public auth: AuthService
     ) {}
 
   ngOnInit(): void {
@@ -24,9 +23,4 @@ export class NavComponent implements OnInit {
     this.modal.toggleModal('auth')
   }
 
-  async logout($event: Event) {
-    $event.preventDefault()
-
-    await this.afAuth.signOut()
-  }
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -10,13 +12,14 @@ import IUser from 'src/app/models/user.model';
 })
 export class RegisterComponent {
   constructor(
-    private auth: AuthService
-  ) {}
+    private auth: AuthService,
+    private emailTaken: EmailTaken
+    ) {}
 
-  inSubmission = false
+  inSubmission = false;
 
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl('', [Validators.required, Validators.email], [this.emailTaken.validate]);
   age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
@@ -24,7 +27,7 @@ export class RegisterComponent {
   ]);
   password = new FormControl('', [
     Validators.required,
-    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+    Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
   ]);
   confirm_password = new FormControl('', [Validators.required]);
   phoneNumber = new FormControl('', [
@@ -33,37 +36,40 @@ export class RegisterComponent {
     Validators.maxLength(9),
   ]);
 
-  showAlert = false
-  alertMsg = 'Your account is beaing created.'
-  alertColor = 'blue'
+  showAlert = false;
+  alertMsg = 'Your account is beaing created.';
+  alertColor = 'blue';
 
-  registerForm = new FormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   async register() {
-    this.showAlert = true
-    this.alertMsg = 'Your account is being created.'
-    this.alertColor = 'blue'
-    this.inSubmission = true
+    this.showAlert = true;
+    this.alertMsg = 'Your account is being created.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
 
     try {
-      await this.auth.createUser(this.registerForm.value as IUser)
+      await this.auth.createUser(this.registerForm.value as IUser);
     } catch (e) {
-      console.error(e)
+      console.error(e);
 
-      this.alertMsg = 'An unexpected error occured. Please try again later.'
-      this.alertColor = 'red'
-      this.inSubmission = false
+      this.alertMsg = 'An unexpected error occured. Please try again later.';
+      this.alertColor = 'red';
+      this.inSubmission = false;
       return;
     }
 
-    this.alertMsg = 'Success! Your account has been created.'
-    this.alertColor = 'green'
+    this.alertMsg = 'Success! Your account has been created.';
+    this.alertColor = 'green';
   }
 }
